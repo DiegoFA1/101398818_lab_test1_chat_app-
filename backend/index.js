6822deae6b9041e03e1a6389570c95bd5ad83f5b
DiegoFA1 <144127274+DiegoFA1@users.noreply.github.com>
2024-02-07 19:37:53 -0500

    port changes

--- backend/index.js
+++ backend/index.js
@@ -70,10 +70,17 @@ io.on('connection', (socket) => {
   
 });
 
-
-
-
-const SERVER_PORT = 8090;
+const SERVER_PORT = process.env.PORT || 8090;
 server.listen(SERVER_PORT, () => {
   console.log(`Server running at http://localhost:${SERVER_PORT}/`);
 });
+
+const path = require('path')
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
